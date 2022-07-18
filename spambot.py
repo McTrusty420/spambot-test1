6f9120afff303e4c87d49a00b37f3d820c51db8e
McTrusty <50515040+McTrusty420@users.noreply.github.com>
2022-07-18 20:18:49 +0530

    Update spambot.py

--- spambot.py
+++ spambot.py
@@ -2,23 +2,24 @@ try :
     import pyautogui, time, re
 except :
     print("pyautogui is missing")
-a=input("string")
-b=int(input("repetition"))
-c=int(input("sleep time"))
-d=input("separator")
-if a == "file[s]":
-    g=input("path/filename")
-time.sleep(c)
-for i in range(1,b+1):
-    if a == "file[s]":
-        e=open(g,"r",encoding="UTF-8")
-        for j in e :
-            h=re.split(d,j)
-        for s in h:
-            pyautogui.typewrite(s)
+spam_input=input("string")
+repetition=int(input("repetition"))
+time_interval=int(input("sleep time"))
+separator=input("separator")
+split_text=""
+if spam_input == "file":
+    file=input("path/filename")
+time.sleep(time_interval)
+for reps in range(1,repetition+1):
+    if spam_input == "file":
+        file_opened=open(file,"r",encoding="UTF-8")
+        for split_words in file_opened :
+            split_text=re.split(separator,)
+        for words in split_text:
+            pyautogui.typewrite(words)
             pyautogui.press("enter")
     else:
-        f=re.split(d,a)
-        for s2 in f:
-            pyautogui.typewrite(s2)
+        final_string=re.split(separator,spam_input)
+        for spam in final_string:
+            pyautogui.typewrite(spam)
             pyautogui.press("enter")
